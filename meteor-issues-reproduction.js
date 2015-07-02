5c5b7217726cae78c1f195d92edb815f90323983
shadysayed <shady.sayed@tagipedia.com>
2015-07-03 00:56:34 +0200

    a reproduction for issue 485 of angular meteor

--- meteor-issues-reproduction.js
+++ meteor-issues-reproduction.js
@@ -1,23 +1,73 @@
+Items = new Mongo.Collection("items");
+
+///////////// Add updatedAt/createdAt properties
+Items.before.insert(function(userId, doc) {
+  doc.createdAt = new Date();
+  doc.updatedAt = doc.createdAt;
+});
+Items.before.update(function(userId, doc, fieldNames, modifier, options){
+  if (modifier.$set == null) {
+      modifier.$set = {};
+    }
+    return modifier.$set.updatedAt = new Date();
+});
+
+Items.allow({
+  insert: function(){
+    return true;
+  },
+  update: function(){
+    return true;
+  },
+  remove: function(){
+    return true;
+  }
+});
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  angular.module('meteorIssues', ['angular-meteor'])
+    .controller('SampleCtrl', [
+      '$scope', '$timeout', function($scope, $timeout){
+        $scope.$meteorSubscribe('items').then(function(){
+          //Just get an item
+          $scope.item = $scope.$meteorObject(Items, Items.find({}).fetch()[0]._id)
+          // Simulate user editing in a few seconds
+          $timeout(function(){
+            //an infinite update will happen after the following line because the client and server will keep disagreeing on the value of updatedAt.
+            $scope.item.name = "changed name";
+          }, 3000);
+        });
+      }
+    ]);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+
+  //Logging messages to console stuff
+  var _send,
+  slice = [].slice;
+
+  _send = Meteor.connection._send;
+
+  Meteor.connection._send = function() {
+    console.log('send', arguments);
+    _send.call.apply(_send, [this].concat(slice.call(arguments)));
+  };
+
+  Meteor.connection._stream.on('message', function(message) {
+    console.log('receive', JSON.parse(message));
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Items.find({}).count() === 0) {
+      Items.insert({name: 'Some item'})
+    }
+  });
+
+  Meteor.publish("items", function(){
+    return Items.find({});
   });
 }
